a9b13703258d043fe338013d348f448a0689f4a3
s4na <appletea.umauma@gmail.com>
2021-08-10 22:13:38 +0900

    PostForm作成

---
+++ app/forms/post_form.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+class PostForm
+  include ActiveModel::Model
+
+  attr_accessor :title, :content
+
+  validates :title, presence: true
+
+  def initialize(post: Post.new, **attributes)
+    @post = post
+    attributes ||= default_attributes
+    super(attributes)
+  end
+
+  def save
+    return if invalid?
+
+    ActiveRecord::Base.transaction do
+      post.update!(title: title, content: content)
+    end
+  rescue ActiveRecord::RecordInvalid
+    false
+  end
+
+  def to_model
+    post
+  end
+
+  private
+
+  attr_reader :post
+
+  def default_attributes
+    {
+      title: post.title,
+      content: post.content
+    }
+  end
+end
